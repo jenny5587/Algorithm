a2dbbb90f59f4a2398460c59f2d1c79aeabf18ed
Minheejung <103649749+jenny5587@users.noreply.github.com>
2024-08-18 22:07:03 +0900

    Delete programmers directory

--- programmers/lv0/231119_배열 두 배 만들기.py
+++
@@ -1,9 +0,0 @@
-def solution(numbers):
-    answer = []
-    for i in numbers:
-        answer.append(i * 2)
-    return answer
-
-#cleancode
-def solution(numbers):
-    return list(map(lambda x: x * 2, numbers))
